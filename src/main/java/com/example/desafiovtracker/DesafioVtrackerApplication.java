4d45c7ef52610253b9bb950ba5d12d6431f8c93f
Guilherme Lopes <guilhermelopes17@hotmail.com>
2022-10-22 15:22:20 -0300

    iniciando projeto

---
+++ src/main/java/com/example/desafiovtracker/DesafioVtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafiovtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioVtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioVtrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/desafiovtracker/DesafioVtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafiovtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioVtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
